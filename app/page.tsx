8885a064e09a06fddadab0c79415d6c06f9dd78c
Linkhant19 <lkk19@bu.edu>
2025-04-17 17:13:02 -0400

    removed apos in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <p className="text-sm p-10 pl-50 pr-50 text-blue-700 dark:text-sky-400 font-mono font-semibold">
-        is your url too long? are there many many unnecessary characters that you don't need in that little link of yours? 
-        are you stressed that your link wouldn't fit into a little text box? 
+        is your url too long? are there many many unnecessary characters that you do not need in that little link of yours? 
+        are you stressed that your link would not fit into a little text box? 
         are you just tired of typing the same link over and over again?
         do you want some fun and short link without all the unnecessary text and characters?
         what do these characters mean anyway? who is typing all of these? you want to be unique.
         you want to be cool. right? what was the problem? yes your url is too long.
-        is your url too long? are there many many unnecessary characters that you don't need in that little link of yours? 
-        are you stressed that your link wouldn't fit into a little text box? 
+        is your url too long? are there many many unnecessary characters that you do not need in that little link of yours? 
+        are you stressed that your link would not fit into a little text box? 
         are you just tired of typing the same link over and over again?
         do you want some fun and short link without all the unnecessary text and characters?
         what do these characters mean anyway? who is typing all of these? you want to be unique.
