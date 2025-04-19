61c907b167769fc0f575ab6bd22b4fcafdcad93b
Linkhant19 <lkk19@bu.edu>
2025-04-18 21:57:24 -0400

    fixed some issues

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -3,8 +3,8 @@
 import { redirect, notFound } from "next/navigation";
 import getCollection, { URL_COLLECTION } from "@/db";
 
-export default async function AliasPage({ params }: { params: { alias: string } }) {
-    const { alias } = params;
+export default async function AliasPage({ params }: { params: Promise<{ alias: string }> }) {
+    const { alias } = await params;
 
     const collection = await getCollection(URL_COLLECTION);
     const record = await collection.findOne({ alias });
